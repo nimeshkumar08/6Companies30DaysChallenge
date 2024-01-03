58d0aa059ff685316e506bcb83d1b242c24e3612
Nimesh Kumar <nimeshkumar180@gmail.com>
2024-01-03 23:58:38 +0530

    2nd day

---
+++ miss_repeat.cpp
@@ -0,0 +1,21 @@
+class Solution{
+public:
+    vector<int> findTwoElement(vector<int> arr, int n) {
+        // code here
+        int ans[n+1] = {0};
+        int repeat =0,miss=0;
+        
+        for(int i=0;i<n;i++){
+            ans[arr[i]]++;
+        }
+        for(int i=1;i<n+1;i++){
+            if(ans[i]==2){
+                repeat = i;
+            }
+            else if(ans[i]==0){
+                miss = i;
+            }
+        }
+        return {repeat,miss};
+    }
+}
